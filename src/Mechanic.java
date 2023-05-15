4f0a69507b3392412dc3abd4302d44a56faa4919
ETHMUNI <sense1xnft@gmail.com>
2023-05-15 09:57:33 +0200

    updated Mechanic class

--- src/Mechanic.java
+++ src/Mechanic.java
@@ -1,6 +1,25 @@
-public class Mechanic extends User{
+public class Mechanic{
+
+    private String Id;
+    private String username;
+    private String password;
+    private int rights;
+    User user;
+    private boolean status = true;
     public Mechanic(String id, String username, String password, int rights) {
-        super(id, username, password, rights);
+        this.Id = id;
+        this.username = username;
+        this.password = password;
+        this.rights = rights;
     }
 
+    // status of mechanic
+   public boolean getMechanicStatus() {
+        if(status) {
+           System.out.println(user.getUsername() + "er ledig");
+       } else if (!status) {
+            System.out.println(user.getUsername() + "er ikke ledig");
+        }
+        return status;
+   }
 }
