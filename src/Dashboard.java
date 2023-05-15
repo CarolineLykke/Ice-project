00143b77aca3732f0eadfaa32f4a9533b8e01267
CarolineLykke <carolinelh@live.dk>
2023-05-15 09:21:59 +0200

    h

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,4 +1,18 @@
 public class Dashboard {
 
-hehehehe
+    public boolean login(String username, String password, String id) {
+        if (username == null || password == null) {
+            return false;
+        }
+        for (User user : users) {
+            if (user.getName() != null && user.getPassword() != null
+                    && user.getName().equals(username) && user.getPassword().equals(password)) {
+                currentUser = user.getName();
+                currentId = user.getId();
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
