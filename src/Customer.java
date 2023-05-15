39975779b1d42686a1abccedb873c32350ac3b89
CarolineLykke <carolinelh@live.dk>
2023-05-15 08:56:39 +0200

    loging

--- src/Customer.java
+++ src/Customer.java
@@ -104,6 +104,62 @@ public class Customer  {
 
         return customers;
     }
+
+    public static void getChangesToTheCar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            //STEP 1: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 2: Open a connection
+            System.out.println("Connecting to database loading SavedMovies");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: Execute a query
+            System.out.println("Creating statement...");
+            String sql = "select * from movies join watchedmovies on movies.id=watchedmovies.moviesid";
+            stmt = conn.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 4: Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+
+                String userID = rs.getString("UserID");
+                String movieID = rs.getString("name");
+
+                if (UserHandler.getId().equals(userID)) {
+                    System.out.println(movieID);
+                }
+            }
+            //STEP 5: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+
+        }
+    }
     //se min bil
-    //hvad er der sket med min bil
+
 }
