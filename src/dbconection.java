50ccbb90e83f1cbb22748c4d2dfb66c1904398a7
altinkaya <124771353+altinkaya@users.noreply.github.com>
2023-05-17 15:28:17 +0200

    alt merget

--- src/dbconection.java
+++ src/dbconection.java
@@ -4,9 +4,9 @@ import java.sql.PreparedStatement;
 
 public class dbconection {
 
-    static final String DB_URL = "jdbc:mysql://cp03.nordicway.dk:3306/edk_ice";
+    static final String DB_URL = "jdbc:mysql://e30.dk:3306/edk_ice";
 
     //  Database credentials
     static final String USER = "edk_mustafa";
-    static final String PASS = "CkNhx-6t(S_V";
+    static final String PASS = "xLsMc1P}$l[y";
 }
