b72cf3bc267a5e4e54b9b6c598efff4f705be734
altinkaya <124771353+altinkaya@users.noreply.github.com>
2023-05-17 16:25:28 +0200

    loging

--- src/Customer.java
+++ src/Customer.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer  {
-    static final String DB_URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7617247";
-
-    //  Database credentials
-    static final String USER = "sql7617247";
-    static final String PASS = "nQyBL7eZqc";
 
     private String email;
     private String name;
